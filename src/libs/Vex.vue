b25e5c3564362831905bef67719f2b0bd1758557
Alexey Zimin <pyksik@gmail.com>
2019-06-25 11:37:18 +0300

    FEAT colors gantt

--- src/libs/Vex.vue
+++ src/libs/Vex.vue
@@ -1,8 +1,10 @@
 <template>
-  <span :style="{transform: `scale(${factor[r - 1]}, 1`}"></span>
+  <span :style="[{'transform': `scale(${factor[r - 1]}, 1`}, {'background-color': color[Math.trunc(r / 4)]}]"></span>
 </template>
 <script>
-const factorMartix = [4, 3, 4, 3, 6, 2, 8, 3, 4, 6, 2, 1, 5, 4, 1, 3, 2, 2, 4, 1, 3, 4, 3, 4, 1, 2, 1, 2, 1, 0.5]
+const factorMartix = [4, 3, 4, 3, 6, 2, 8, 3, 4, 6, 2, 1, 5, 4, 1, 3, 2, 2, 4, 1, 3, 4, 7, 4, 5, 3, 2, 2, 1, 0.5]
+const colorMartix = ['#4285f4', '#34a853', '#fbbc05', '#ea4335', '#00bcd4', '#9c27b0', '#795548']
+
 export default {
   name: 'DragDrop',
   props: {
@@ -15,7 +17,8 @@ export default {
   },
   data () {
     return {
-      factor: []
+      factor: [],
+      color: colorMartix
     }
   },
   mounted () {
