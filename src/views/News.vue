7b9c2b4e98cc634eef56d077a79f810170008b2e
Alexey <pyksik@gmail.com>
2019-07-19 14:22:20 +0300

    FEAT wakatime

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,3 +1,12 @@
 <template>
-  <h1><br><br>Здесь могли быть новости или ваша реклама, но пока ни того ни другого :)</h1>
+  <div>
+    <h1><br><br>Здесь могли быть новости или ваша реклама, но пока ни того ни другого :)</h1>
+    <h2><br>Пока здесь можно последить за моей активностью.</h2>
+    <figure><embed src="https://wakatime.com/share/@cbea2116-1b02-482a-a5a3-a74fb1dcb77e/cddfcabb-5ba9-4acc-9579-43c9097c8db4.svg"></embed></figure>
+  </div>
 </template>
+<style lang="stylus" scoped>
+figure
+  max-width 800px
+  margin auto
+</style>
